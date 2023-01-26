eb631705aa3feb17d2c5324a3159a701b6ec1c54
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-26 15:25:16 +0100

    penso si a posto

--- mail/JS/mail.js
+++ mail/JS/mail.js
@@ -6,3 +6,30 @@
 // creo una var riutilizzabile che mi restituisca array[i]
 // if prompt input  viene trovato messaggio di benvenuto in caso contrario aler di errore
 
+const mailList = [
+    "pinco@pallino.it", //indice 0
+    "giovannibianchi@libero.it", // indice 1
+    "lasagne.fumanti@libero.it", // indice 2
+    "renatozero@gmail.com", // indice 3
+    "vasco.rossi@yahoo.it", // indice 4
+    "buddy.velastro@cakedesign.us" // indice 5
+];
+
+const emailInput = prompt("inserisci la tua mail");
+
+let outputMessage = document.getElementById("screen-message")
+
+
+for (let i = 0; i < mailList.length; i++ ){
+    let currentEl = mailList[i];
+    
+
+
+    if (emailInput == currentEl)
+    {
+       outputMessage.innerHTML = "Non sei stato riconosciuto dal sistema,ti preghiamo di riprovare";
+        
+    
+       outputMessage.innerHTML = "Benvenuto," + currentEl;
+    }}
+    
