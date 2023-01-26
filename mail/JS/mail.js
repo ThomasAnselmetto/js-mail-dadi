5a93382c9fa1aea964f60441225873fefe3d071f
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-26 14:41:46 +0100

    init commit

---
+++ mail/JS/mail.js
@@ -0,0 +1,8 @@
+// creo un array con all'interno x lista di mail
+// creo un prompt all'interno del quale il cliente puo' inserire la sua mail
+// devo ricordarmi di dare al prompt un parse se voglio che il prompt mi restitutisca un int e non una stringa
+// creo un ciclo per analizzare gli input dati dal cliente
+// dovro' affidare a "i" di essere cercato fino alla fine degli elementi dell'array
+// creo una var riutilizzabile che mi restituisca array[i]
+// if prompt input  viene trovato messaggio di benvenuto in caso contrario aler di errore
+
